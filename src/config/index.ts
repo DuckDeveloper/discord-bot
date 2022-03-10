3af3f732051a65064983e32190a1f9c3973f141f
VSabirov <v.sabirov@wptt.ru>
2022-03-10 20:39:00 +0500

    initial

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 const token = process.env.token
 const YOUTUBE_API_KEYS = JSON.parse(process.env.YOUTUBE_API_KEYS)
-
+console.log(process.env.YOUTUBE_API_KEYS, YOUTUBE_API_KEYS)
 const prefix = '!'
 const YOUTUBE_URL = 'https://youtube.com/embed'
 const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search'
